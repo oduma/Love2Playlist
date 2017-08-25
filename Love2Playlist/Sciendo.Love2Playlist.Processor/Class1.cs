f497de9b7e6262183645dfcc14d4494fa21f4479
oduma <tavi_duma2002@yahoo.co.uk>
2017-08-25 18:06:52 +0100

    Initial commit

---
+++ Love2Playlist/Sciendo.Love2Playlist.Processor/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sciendo.Love2Playlist.Processor
+{
+    public class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+}
